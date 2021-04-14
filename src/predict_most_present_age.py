4fa472a9f6f97a81eecff56001ef32475808daff
Isaac Cheng <isaaccheng9@gmail.com>
2021-04-14 17:12:53 +0100

    Read data set from CSV file

--- src/predict_most_present_age.py
+++ src/predict_most_present_age.py
@@ -2,11 +2,17 @@
 An implementation of a decision tree classifier to predict the most present age
 in each urban area in the data set.
 """
+import pandas
 
 
 def main():
-    pass
+    # Loads the data set from the CSV file.
+    data = pandas.read_csv("resources/data.csv")
+    data.head()
+    # Gets all column names from the data set except the most_present_age.
+    feature_columns = list(data.columns)
+    feature_columns.remove("most_present_age")
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
