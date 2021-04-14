9e58c7d969dc185586f3233ad989ff4d9f9c11e0
Isaac Cheng <isaaccheng9@gmail.com>
2021-04-14 17:41:20 +0100

    Build the decision tree model

--- src/predict_most_present_age.py
+++ src/predict_most_present_age.py
@@ -3,6 +3,8 @@ An implementation of a decision tree classifier to predict the most present age
 in each urban area in the data set.
 """
 import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 
 def main():
@@ -12,6 +14,19 @@ def main():
     # Gets all column names from the data set except the most_present_age.
     feature_columns = list(data.columns)
     feature_columns.remove("most_present_age")
+
+    # Splits the data set in feature variables and the target variable.
+    features = data[feature_columns]
+    target = data.most_present_age
+    (feature_train, feature_test,
+     target_train, target_test) = train_test_split(features, target,
+                                                   test_size=0.3,
+                                                   random_state=1)
+    # Create the decision tree classifier and train it.
+    classifier = DecisionTreeClassifier()
+    classifier = classifier.fit(feature_train, target_train)
+    target_prediction = classifier.predict(feature_train)
+    print(target_prediction)
 
 
 if __name__ == "__main__":
