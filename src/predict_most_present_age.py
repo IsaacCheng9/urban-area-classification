cbf37207645602d92438cbf7a9d15be248443c4d
Isaac Cheng <isaaccheng9@gmail.com>
2021-04-14 16:28:49 +0100

    Initial commit

---
+++ src/predict_most_present_age.py
@@ -0,0 +1,12 @@
+"""
+An implementation of a decision tree classifier to predict the most present age
+in each urban area in the data set.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "main":
+    main()
