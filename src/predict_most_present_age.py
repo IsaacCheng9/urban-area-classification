c53c1e121168a84cc45a29189bf8b96570eec976
Isaac Cheng <isaaccheng9@gmail.com>
2021-04-15 01:57:30 +0100

    Add docstrings, refactor code

--- src/predict_most_present_age.py
+++ src/predict_most_present_age.py
@@ -1,6 +1,8 @@
 """
 An implementation of a decision tree classifier to predict the most present age
-in each urban area in the data set.
+in each urban area in the data set. It performs the prediction, and evaluates
+the accuracy of the classifier per maximum depth level, as well as the
+importance of features of urban areas from the data set.
 """
 import json
 from statistics import mean
@@ -13,39 +15,64 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 def main():
+    """
+    Runs the decision tree classification algorithm and evaluation.
+    """
     data = read_data_set()
     feature_columns, features, target = prepare_data_set(data)
     evaluate_classifier_accuracy(features, target)
-    # Identifies influence of features in most present age of an urban area.
-    visualise_feature_importance(feature_columns, features, target)
+    evaluate_feature_importance(feature_columns, features, target)
 
 
 def read_data_set():
-    # Loads the data set from the CSV file.
+    """
+    Loads the data set from the CSV file.
+
+    Returns:
+        The data set of Rome from the CSV file.
+    """
     data = pandas.read_csv("resources/data.csv")
     data.head()
     return data
 
 
 def prepare_data_set(data):
+    """
+    Prepares the data set to use with the decision tree classifier.
+
+    Args:
+        data: The data set of Rome from the CSV file.
+
+    Returns:
+        Attributes to use in the decision tree and the attribute to predict.
+    """
     # Gets all column names from the data set except cell_id and
     # most_present_age.
     feature_columns = list(data.columns)
     feature_columns.remove("cell_id")
     feature_columns.remove("most_present_age")
-    # Splits the data set in feature variables and the target variable.
     features = data[feature_columns]
     target = data.most_present_age
     return feature_columns, features, target
 
 
 def train_decision_tree(depth, features, target):
+    """
+    Creates the decision tree classifier and trains it.
+
+    Args:
+        depth: Maximum depth level for the decision tree.
+        features: The attributes to use in the decision tree.
+        target: The attribute to predict.
+
+    Returns:
+        The decision tree classifier, and its accuracy.
+    """
     # Uses 40% of the data set for the test.
     (feature_train, feature_test,
      target_train, target_test) = train_test_split(features, target,
                                                    test_size=0.4,
                                                    random_state=1)
-    # Create the decision tree classifier and train it.
     classifier = DecisionTreeClassifier(max_depth=depth)
     classifier = classifier.fit(feature_train, target_train)
     target_prediction = classifier.predict(feature_test)
@@ -55,8 +82,14 @@ def train_decision_tree(depth, features, target):
 
 
 def evaluate_classifier_accuracy(features, target):
-    # Checks the accuracy of the decision tree classifier with different depths
-    # and test sizes.
+    """
+    Checks the accuracy of the decision tree classifier with different depths
+    and test sizes.
+
+    Args:
+        features: The attributes to use in the decision tree.
+        target: The attribute to predict.
+    """
     depth_accuracy_dict = {}
     # Gets the accuracy of five analysis runs for each maximum depth of
     # decision tree classifier.
@@ -69,7 +102,7 @@ def evaluate_classifier_accuracy(features, target):
             except KeyError:
                 depth_accuracy_dict[key] = [accuracy]
     # Calculates the average accuracy for each maximum depth setting.
-    for key in depth_accuracy_dict.keys():
+    for key in depth_accuracy_dict:
         depth_accuracy_dict[key] = mean(depth_accuracy_dict[key])
     # Pretty prints the average accuracy values.
     print(json.dumps(depth_accuracy_dict, indent=4) + "\n\n")
@@ -87,7 +120,15 @@ def evaluate_classifier_accuracy(features, target):
     plt.show()
 
 
-def visualise_feature_importance(feature_columns, features, target):
+def evaluate_feature_importance(feature_columns, features, target):
+    """
+    Identifies influence of features for most present age of an urban area.
+
+    Args:
+        feature_columns: Listed attributes used in the decision tree.
+        features: The attributes to use in the decision tree.
+        target: The attribute to predict.
+    """
     # Generates a bar chart to visualise feature importance.
     classifier, accuracy = train_decision_tree(10, features, target)
     feature_importance = classifier.feature_importances_
